0cf5a21a5d7751211ceaea30ee6f4ffed8fc9bd1
astr4vale <richardarenaza301@gmail.com>
2025-12-01 18:37:35 -0500

    Add a print statement to asgi.py for commit tracking

--- ioteca_api/ioteca_main/asgi.py
+++ ioteca_api/ioteca_main/asgi.py
@@ -13,4 +13,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ioteca_main.settings')
 
+
+print("Este es mi commit a asgi.py - Valentin Arenaza")
+
 application = get_asgi_application()
