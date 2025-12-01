e9f31221192bf7391fb277042e707dfa779fd8c2
lucero154 <lucerocruz983@gmail.com>
2025-12-01 18:41:22 -0500

    Add message for Django admin execution
    Added a print statement to indicate Django admin execution.
    agregué un print informativo

--- ioteca_main/manage.py
+++ ioteca_main/manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,6 +5,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print("🔧 Ejecutando el administrador de Django...")  # ← Pequeña modificación
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ioteca_main.settings')
     try:
         from django.core.management import execute_from_command_line
