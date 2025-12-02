c6545ad57ce4abab9678c44477a40c1e01144d72
astr4vale <richardarenaza301@gmail.com>
2025-12-01 19:36:50 -0500

    Remove merge conflict markers and add a commit message print statement in asgi.py

--- ioteca_api/ioteca_main/asgi.py
+++ ioteca_api/ioteca_main/asgi.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 """
 ASGI config for ioteca_main project.
 
@@ -15,5 +13,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ioteca_main.settings')
 
+
+print("Este es mi commit a asgi.py - Valentin Arenaza")
+
 application = get_asgi_application()
->>>>>>> adf3ec2066e4c860753a972837daab924ff7f6f2
